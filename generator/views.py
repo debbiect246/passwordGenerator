334d327e475fa35250876e1aaddd1deb47c2e976
Debbie Thompson <deb246@btinternet.com>
2020-04-18 21:08:16 +0100

    added password page and submit button which connects to password page

--- generator/views.py
+++ generator/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from generator import views
 
 
 def home(request):
-    return render(request, 'generator/home.html', {"password": "abcdefg"})
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    return render(request, 'generator/password.html')
