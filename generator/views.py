c67eba797d30f701566727f8780da63a4fb9726a
Debbie Thompson <deb246@btinternet.com>
2020-04-22 19:58:09 +0100

    added about page

--- generator/views.py
+++ generator/views.py
@@ -10,6 +10,10 @@ def home(request):
     return render(request, 'generator/home.html')
 
 
+def about(request):
+    return render(request, 'generator/about.html')
+
+
 def password(request):
     # list of characters to chose from
     characters = list('abcdefghijklmnopqrstuvwxyz')
